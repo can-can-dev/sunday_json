bca85a17c4ddc0af54ca2388c3246aebd41237d5
renkon1103 <renkon1103@gmail.com>
2019-09-29 02:20:43 +0900

    db_access class化

--- db_access.php
+++ db_access.php
@@ -5,15 +5,20 @@
   $dotenv = Dotenv\Dotenv::create(__DIR__);
   $dotenv->load();
  
-  // ドライバ呼び出しを使用して MySQL データベースに接続します
-  $dsn = sprintf('mysql:dbname=%s;host=%s', $_ENV['DB_TABLE'], $_ENV['DB_HOST']);
-  $user = $_ENV['DB_USER'];
-  $password = $_ENV['DB_PASSWORD'];
 
-  try {
-      $dbh = new PDO($dsn, $user, $password);
-      echo "接続成功\n";
-  } catch (PDOException $e) {
-      echo "接続失敗: " . $e->getMessage() . "\n";
-      exit();
-  } 
+  class db_access {
+    public function access_check(){
+      // ドライバ呼び出しを使用して MySQL データベースに接続します
+      $dsn = sprintf('mysql:dbname=%s;host=%s', $_ENV['DB_TABLE'], $_ENV['DB_HOST']);
+      $user = $_ENV['DB_USER'];
+      $password = $_ENV['DB_PASSWORD'];
+
+      try {
+        $dbh = new PDO($dsn, $user, $password);
+        echo "接続成功\n";
+      } catch (PDOException $e) {
+          echo "接続失敗: " . $e->getMessage() . "\n";
+          exit();
+      }
+    }
+  }
